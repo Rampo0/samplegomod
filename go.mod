8e67494999250cb61d23b14d19f58f404a1d554a
rampo0 <36811859+Rampo0@users.noreply.github.com>
2020-09-14 17:00:54 +0700

    ad

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rampo0/samplegomod
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.6.3
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/rampo0/samplegomod/testpackage"
 )
 
 func main() {
 	testpackage.HelloWorld()
+
+	router := gin.Default()
+	router.Run(":8080")
 }
